5483c1eb8973032ac6d574fa87056a7e1c1fbfe1
SonuSharma000BazarKhojo <info@boost_up.in>
2024-03-05 00:23:29 +0530

    Page Routing Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './Pages/About'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
 
 const App = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/login' element={<SignIn/>}></Route>
+    <Route path='/signup' element={<SignUp/>}></Route>
+    <Route path='/profile' element={<Profile/>}></Route>
+    <Route path='/about' element={<About/>}></Route>
+  </Routes>
+  </BrowserRouter>
 }
 
 export default App
