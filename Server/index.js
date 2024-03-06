1844e7eef377260944bffb71e7f75e2e4c1feb0e
SonuSharma000BazarKhojo <info@boost_up.in>
2024-03-06 23:27:27 +0530

    MongoDB Atlas Connected

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("mongo Connected");
+}).catch((err)=>{
+    console.log(err);
+})
+
+
 const app = express();
+
 
 app.listen(3000,()=>{
     console.log('Server is running at port 3000')
